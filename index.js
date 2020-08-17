90cab575ab58fa0ec374939a72338dde30987e55
Asma Kolaib <as.kolaib@gmail.com>
2020-08-17 06:04:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Formatter {
     static capitalize(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
-    static sanitize(stringing) {
+    static sanitize(string) {
         return string.replace(/[^A-Za-z0-9- ']+/g, "");
     }
     static titleize(string) {
